80792d31b4d85cf5a9f9a23b8e9bc6aa51f7db19
ShaMaxDev <maksimbychkov@yahoo.com>
2023-09-15 22:23:25 -0700

    Add project files.

---
+++ Exit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string usertInput = " ";  
+            Console.WriteLine("Input 'exit' to exit or somethimg else for something else");
+            while (usertInput != "exit") 
+            {
+                usertInput = Console.ReadLine();
+            }
+        }
+    }
+}
